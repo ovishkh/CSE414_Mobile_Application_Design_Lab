d12a039933ccfa698fe474ec4d24f554d551c43f
Ovi <133152464+ovishkh@users.noreply.github.com>
2026-01-26 09:37:41 +0600

    Labs - 26/01/26

---
+++ lab_2.2_contrsutor.dart
@@ -0,0 +1,17 @@
+class avenger(){
+
+    avenger(){
+        print("I am an Avenger");
+    }
+
+    first_avenger(){
+        print("I am Ironman");
+    }
+
+
+}
+
+void main(){
+    var Tony = new avenger();
+    Tony.first_avenger();
+}
